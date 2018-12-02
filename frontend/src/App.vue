e6e9630befd362b639aed031c54979a0aee7c4e6
Aron Richter <aronrichter@gmail.com>
2018-12-02 09:21:03 -0200

    Inicio projeto fronend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <toolbar titulo="Emissão Pedidos"></toolbar>
+  </div>
+</template>
+
+<script>
+import toolbar from './components/toolbar.vue';
+
+export default {
+  components: {
+    'toolbar': toolbar,
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero is-dark">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{ titulo }}</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['titulo'],
+}
+</script>
+
+<style>
+  
+</style>
+
+
