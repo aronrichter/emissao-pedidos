212e0e2f30032719123c0a5599ad5000b53055fa
Aron Richter <arichter@fesctecnologia.com.br>
2018-12-04 13:49:40 -0200

    Adição VueRouter

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import { Table } from 'buefy/dist/components/table'
 import { Input } from 'buefy/dist/components/input'
 import 'buefy/dist/buefy.css'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 Vue.component('b-table', Table)
 Vue.component('b-input', Input)
 
