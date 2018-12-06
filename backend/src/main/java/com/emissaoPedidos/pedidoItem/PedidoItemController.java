1f8b7f49f8e0cd6d0a085718a7cac09682c3a930
Aron Richter <arichter@fesctecnologia.com.br>
2018-12-06 16:23:19 -0200

    Ajustes para necesśarios para funcionamento da grid(componente)

--- backend/src/main/java/com/emissaoPedidos/pedidoItem/PedidoItemController.java
+++ backend/src/main/java/com/emissaoPedidos/pedidoItem/PedidoItemController.java
@@ -1,13 +1,11 @@
 package com.emissaoPedidos.pedidoItem;
 
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestController
 @RequestMapping("/pedidosItem")
 public class PedidoItemController {
